802b21f83757ef941b15c534b284321b2212393a
csPython <cspython@AD25TF04NGF1J-17.local>
2019-01-14 10:05:57 -0500

    All Questions Added

--- quizAA.py
+++ quizAA.py
@@ -69,6 +69,73 @@ q4 = """ QA: What Is Akhi's Favorite Subject?
 a4 = int(0)
 check4 = bool(False)
 q4Ans = 1
+
+#question5
+q5= """QA:Whats Akhis Favorite Color?
+1)Pink
+2)Navy Blue
+3)Turquoise
+4)Lavender
+"""
+a5 = int(0)
+check5 = bool(False)
+q5Ans = 2
+
+#question6
+q6=""" QA:What Zodiac Sign Is Akhi? 
+1)Capricorn
+2)Saggitarius
+3)Leo
+4)Cancer
+"""
+a6 = int(0)
+check6 = bool(False)
+q6Ans = 4
+
+#question7
+q7="""QA:Whats Akhis Eye Color
+1)black
+2)dark brown
+3)brown 
+4)light brown 
+"""
+a7 = int(0)
+check7 = bool(False)
+q7Ans = 3
+
+#question8
+q8=""" QA:What Type Of Consol Does Akhi Play?
+1)Xbox
+2)Pc
+3)Wii
+4)Playstation 
+"""
+a8 = int(0)
+check8 = bool(False)
+q8Ans = 4
+
+#question9
+q9="""QA:What College Does Akhi Want To Go To?
+1)Marist
+2)Colombia
+3)NYU
+4)MIT
+"""
+a9 = int(0)
+check9 = bool(False)
+q9Ans = 2
+
+#question10
+q10="""QA:What is Akhis Favorite Type Of Food?
+1)Halal Cart food 
+2)Indial Food
+3)Turkish Food
+4)Chicken 
+"""
+a10 = int(0)
+check10 = bool(False)
+q10Ans = 3
+
 #intro
 print("Welcome to my quiz where you will be asked 10 questions")
 print("Each question will have 4 multiple choces")
@@ -76,20 +143,23 @@ print ("All of these questions will be based on how well you know Akhi")
 print("Good Luck")
 print()
 
-#Question1
+#Question1-4
 run_qa_loop(q1, a1, check1, q1Ans)
-
-#Question2
 run_qa_loop(q2, a2, check2, q2Ans)
-
-#Question3
 run_qa_loop(q3, a3, check3, q3Ans)
-
-#Question4
 run_qa_loop(q4, a4, check4, q4Ans)
+#Question5-8
+run_qa_loop(q5, a5, check5, q5Ans)
+run_qa_loop(q6, a6, check6, q6Ans)
+run_qa_loop(q7, a7, check7, q7Ans)
+run_qa_loop(q8, a8, check8, q8Ans)
+#Question9-10
+run_qa_loop(q9, a9, check9, q9Ans)
+run_qa_loop(q10, a10, check10, q10Ans)
+
 
 #score
-print( "you got", grade *25, "%")
+print( "you got", grade *10, "%")
 
 
 
