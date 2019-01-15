61971f35ce290b89f3f6ab8d5a3f86a4b17a5dc2
csPython <cspython@AD25TF04NGF1J-17.local>
2019-01-15 11:48:03 -0500

    Added data in a seperate file

---
+++ quizAAdata.py
@@ -0,0 +1,112 @@
+ #quizAAdata by akhi abdullalh
+# data for q
+
+#First Question 
+q1 = """QA: Where is Akhi from?
+1) Dominican republic 
+2) Arabic 
+3) India
+4) Bangladesh
+"""
+a1 = int(0)
+check1 = bool(False)
+q1Ans = 4
+
+#question2
+q2 = """QA: Whens Akhis Birthday?
+1) July 11th 
+2) January 4th
+3) April 30th
+4) June 16th 
+"""
+a2 = int(0)
+check2 = bool(False)
+q2Ans = 1
+
+#question3
+q3 = """QA:How Tall is Akhi? 
+1) 5'3
+2) 5'4
+3) 5'5
+4) 5'3.5
+"""
+a3 = int(0)
+check3 = bool(False)
+q3Ans = 3
+
+#Question4
+q4 = """ QA: What Is Akhi's Favorite Subject? 
+1)Math
+2)Science
+3)English
+4)History 
+"""
+a4 = int(0)
+check4 = bool(False)
+q4Ans = 1
+
+#question5
+q5= """QA:Whats Akhis Favorite Color?
+1)Pink
+2)Navy Blue
+3)Turquoise
+4)Lavender
+"""
+a5 = int(0)
+check5 = bool(False)
+q5Ans = 2
+
+#question6
+q6=""" QA:What Zodiac Sign Is Akhi? 
+1)Capricorn
+2)Saggitarius
+3)Leo
+4)Cancer
+"""
+a6 = int(0)
+check6 = bool(False)
+q6Ans = 4
+
+#question7
+q7="""QA:Whats Akhis Eye Color
+1)black
+2)dark brown
+3)brown 
+4)light brown 
+"""
+a7 = int(0)
+check7 = bool(False)
+q7Ans = 3
+
+#question8
+q8=""" QA:What Type Of Consol Does Akhi Play?
+1)Xbox
+2)Pc
+3)Wii
+4)Playstation 
+"""
+a8 = int(0)
+check8 = bool(False)
+q8Ans = 4
+
+#question9
+q9="""QA:What College Does Akhi Want To Go To?
+1)Marist
+2)Colombia
+3)NYU
+4)MIT
+"""
+a9 = int(0)
+check9 = bool(False)
+q9Ans = 2
+
+#question10
+q10="""QA:What is Akhis Favorite Type Of Food?
+1)Halal Cart food 
+2)Indial Food
+3)Turkish Food
+4)Chicken 
+"""
+a10 = int(0)
+check10 = bool(False)
+q10Ans = 3
